1e7e5d306a3828268d6f5a8f65033df59c4e6c6d
Vitaliy Blats <biznika@gmail.com>
2024-02-09 02:56:08 +0200

    Update example_tray_client.php

--- systray/example_tray_client.php
+++ systray/example_tray_client.php
@@ -4,7 +4,7 @@ global $old_mtime, $mtime, $window;
 
 function add_me_to_tray($my_name, $my_icon, $tooltip, $pixbuf=true) {
 	global $mtime;
-// Если директории в формате /run/user/CURRENT_USER/systray/PID не существует, то создаем ее
+// Если директории в формате /run/user/CURRENT_USER/PID/systray не существует, то создаем ее
 	if (!is_dir($_SERVER['XDG_RUNTIME_DIR']."/systray/".getmypid())) {
 		mkdir($_SERVER['XDG_RUNTIME_DIR']."/systray/".getmypid(),0700,true);
 	}
@@ -26,7 +26,7 @@ function get_action() {
 }
 function check_for_actions($window)
 {
-	global $old_mtime, $mtime, $context_menu;
+	global $old_mtime, $mtime, $context_menu, $menu, $vbox;
 	$old_mtime = $mtime;	
 	$mtime = filectime($_SERVER['XDG_RUNTIME_DIR']."/systray/".getmypid()."/.updated");
 	if ($mtime!=$old_mtime) { // Если файл изменился - значит произошло событие
@@ -38,51 +38,34 @@ function check_for_actions($window)
 			$window->present_with_time($time);
 			$window->show();
 		}
-		if (get_action()=="ContextMenu") { // Создаем простейшее контекстное меню. Реализовано в форме окна, поэтому не закрывается по потере фокуса
-			$display = new GdkDisplay();
-			global $x,$y;
-			$x = $display->get_mouse_positionX(); // Возле курсора мыши, само собой
-			$y = $display->get_mouse_positionY();
-			unset($display);
-			$context_menu = new GtkWindow();
-			$context_menu->set_type_hint(2);
-			$context_menu->set_size_request(200, 200);
-			$context_menu->set_decorated(false);
-			$vbox = new GtkBox(GtkOrientation::VERTICAL);
-			$vbox->set_border_width(0);
-			$o_button = GtkButton::new_with_label("Open"); // Менюшка Open будет открывать наше окно
-			$o_button->connect("clicked", function() {global $x,$y, $window, $context_menu;$time=time();$window->show();$window->present_with_time($time);$window->activate_focus();$context_menu->destroy();});
-			$vbox->add($o_button);
-			$o_button->show();
-			$button = GtkButton::new_with_label("Close"); // Менюшка Close будет его закрывать.
-			$button->connect("clicked", function() {Gtk::main_quit();});
-			$vbox->add($button);
-			$button->show();
-			$context_menu->add($vbox);
-			$context_menu->show();
-			$vbox->show();
-			$context_menu->move($x+22,$y+22); // Не на иконке же рисовать меню - нарисуем чуть правее и ниже
-			$context_menu->activate_focus();
+		if (get_action()=="ContextMenu") { // Создаем простейшее контекстное меню
+	    $menu->popup_at_pointer();
 		}
 	}
 	clearstatcache(); // Пых кеширует date modify по умолчанию, надо чистить
 }
 Gtk::init();
 $window = new GtkWindow();
-$window->set_size_request(250, 250);
-$window->set_title("SystemTrayExaple");
-$window->set_decorated(true);
+$window->set_size_request(500, 500);
 $vbox = new GtkBox(GtkOrientation::VERTICAL);
-$vbox->set_border_width(1);
-$label = new GtkLabel("Ну типа контент");
-$vbox->add($label);
-$button = GtkButton::new_with_label("Кнобка !");
-$vbox->add($button);
+$menubar = new GtkMenuBar();
+$vbox->pack_start($menubar, FALSE, FALSE, 0);
+$menu = new GtkMenu();
+$menu->append($menu_item=GtkMenuItem::new_with_label("Open"));
+$menu->append($menu_item=GtkMenuItem::new_with_label("Show stats"));
+$menu->append($menu_item=GtkMenuItem::new_with_label("Free memory"));
+$menu->append($menu_item=GtkMenuItem::new_with_label("Quit monitor"));
+$menuitem = new GtkMenuItem();
+$menuitem->set_submenu($menu); // set the sub menu on menuitem
+$menubar->append($menuitem); // append menu About to menubar
+$menu->show_all();
+$vbox->pack_start(new GtkLabel(""), TRUE, TRUE, 10);
 $window->add($vbox);
 $window->connect("destroy", function() {
 	Gtk::main_quit();
 });
 $window->show_all();
+$window->hide();
 // Добавляем нашу программку в трей
 add_me_to_tray("Test App", "viber", "Test App в трее", false);
 // Добавляем таймер для периодического чека
